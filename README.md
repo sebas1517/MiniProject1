8367f4c014e282fc2332dfa0f34872bc61afb469
ssebas1517 <ss61@uic.edu>
2022-01-20 21:49:10 -0600

    Drawing finished,read me filled out

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # MiniProject1
-First P5 Sketch
+
+1. Started with a regular circle in the middle and it grows out evenly everytime.
+2. Left ellipse is drawn with the center on the center of the left window edge. Size is set and it then grows out evenly.
+3. Right ellipse is drawn with the center on the center of the right window edge. Size is set and it then grows out evenly.
+4. Top ellipse is drawn with the center on the center of the top window edge. Size is set and it then grows out evenly.
+5. Bottom ellipse is drawn with the center at the center of the bottom window edge. Size is set and it grows out evenly.
--- sketch.js
+++ sketch.js
@@ -1,7 +1,29 @@
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(windowWidth, windowHeight);
   }
   
   function draw() {
-    background(220,0,0);
+    background(220,100,100);
+    strokeWeight(2);
+    x = 0;
+    y = 0;
+    for(i = 0; i < 50; i++){
+      noFill();
+      x = x +50;
+      y = y+30;
+      circle(windowWidth/2, windowHeight/2, x);
+
+      noFill();
+      ellipse(windowWidth,windowHeight/2,x,y);
+
+      noFill();
+      ellipse((windowWidth-windowWidth),windowHeight/2,x,y);
+
+      noFill();
+      ellipse(windowWidth/2,windowHeight,y,x);
+
+      noFill();
+      ellipse(windowWidth/2,(windowHeight-windowHeight),y,x);
+    }
+
   }
